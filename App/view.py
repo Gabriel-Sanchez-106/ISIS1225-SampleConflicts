97fa206507625493b18ee255e402ac0df26e5852
Gabriel-Sanchez-106 <gs.sanchez@uniandes.edu.co>
2021-02-08 16:24:57 -0500

    Complemento

--- App/view.py
+++ App/view.py
@@ -84,6 +84,8 @@ while True:
 
 
         # TO-DO: Modificación de Est-2 en el Lab 2
+        print('Último libro cargado: ' + str(lt.lastElement(books)))
+
 
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
@@ -91,7 +93,10 @@ while True:
         print('Total de tags cargados: ' + str(lt.size(tags)))
     elif True:
         # TO-DO: Modificación de Est-2 en el Lab 2
-        pass
+        elif int(inputs[0]) == 3:
+        print("Cargando información de Book-Tags...")
+        booktags = loadBooksTags()
+        print('Total de Book-Tags cargados: ' + str(lt.size(booktags)))
 
     else:
         sys.exit(0)
